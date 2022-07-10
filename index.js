f7916180dd89150434c50730cbf9ae6149f2aa0e
james <jamesrobertson@outlook.com>
2022-07-10 10:19:48 +0100

    cron deploy test

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
 const puppeteer = require('puppeteer');
+const cron = require('node-cron');
 const username = "james3";
-const password = "";
+const password = "7efb3dd3";
 
-const roles = [
-    {
-        name: "General Evaluator",
-        id: "I ACCEPT the 'General Evaluator' role"
-    },
-    {
-        name: "1st Evaluator",
-        id: "I ACCEPT the 'Evaluator' role"
-    },
-    {
-        name: "Ah Counter",
-        id: "I ACCEPT the 'Ah Counter' role"
-    },
-    {
-        name: "Grammarian",
-        id: "I ACCEPT the 'Grammarian' role"
-    },
-    {
-        name: "Table Topic Master",
-        id: "I ACCEPT the 'Table Topic Master' role"
-    }
-];
+// const roles = [
+//     {
+//         name: "General Evaluator",
+//         id: "I ACCEPT the 'General Evaluator' role"
+//     },
+//     {
+//         name: "1st Evaluator",
+//         id: "I ACCEPT the 'Evaluator' role"
+//     },
+//     {
+//         name: "Ah Counter",
+//         id: "I ACCEPT the 'Ah Counter' role"
+//     },
+//     {
+//         name: "Grammarian",
+//         id: "I ACCEPT the 'Grammarian' role"
+//     },
+//     {
+//         name: "Table Topic Master",
+//         id: "I ACCEPT the 'Table Topic Master' role"
+//     }
+// ];
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://toastmasterclub.org/');
-    await page.focus('#username');
-    await page.keyboard.type(username);
-    await page.focus('input[name=password]');
-    await page.keyboard.type(password);
+// const findAvailableRoles = (rolesHtml) => {
+//     return ['ah counter'];
+// }
 
-    await page.click('input[type="submit"]');
-    await page.waitForSelector('#ui-id-3', { visible: true })
+// const sendAvailableRolesToPhone = (roles) => {
 
-    const linkToNextMeeting = await page.evaluate('document.querySelector("#ui-id-4 > p:nth-child(2) > a").getAttribute("href")')
-    console.log(linkToNextMeeting);
-    await page.goto(`https://toastmasterclub.org/${linkToNextMeeting}`);
-    await page.click('img[alt="Following meeting"]');
+// }
 
-    await page.screenshot({ path: 'example.png' });
+// (async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://toastmasterclub.org/');
+//     await page.focus('#username');
+//     await page.keyboard.type(username);
+//     await page.focus('input[name=password]');
+//     await page.keyboard.type(password);
 
-    await browser.close();
-})();
+//     await page.click('input[type="submit"]');
+//     await page.waitForSelector('#ui-id-3', { visible: true })
 
-// scrape next meetings available roles
-// message avialable roles
-// listen for incoming sms role choise
-// fill out role selection on TM
+//     const linkToNextMeeting = await page.evaluate('document.querySelector("#ui-id-4 > p:nth-child(2) > a").getAttribute("href")')
+//     console.log(linkToNextMeeting);
+//     await page.goto(`https://toastmasterclub.org/${linkToNextMeeting}`);
+//     await page.click('img[alt="Following meeting"]');
+
+//     await page.screenshot({ path: 'example.png' });
+
+//     await browser.close();
+// })();
 
 
-/* <a href="view_meeting.php?action=volunteer&amp;t=80691&amp;r=4970&amp;n=0&amp;att=1"><img src="templates/TM_Blue/images/lang_english/icon_yes.gif" alt="I ACCEPT the 'Grammarian' role" title="I ACCEPT the 'Grammarian' role" border="0"></a>
-<a href="view_meeting.php?action=volunteer&amp;t=80691&amp;r=4971&amp;n=0&amp;att=1"><img src="templates/TM_Blue/images/lang_english/icon_yes.gif" alt="I ACCEPT the 'Table Topic Master' role" title="I ACCEPT the 'Table Topic Master' role" border="0"></a>
-<a href="view_meeting.php?action=volunteer&amp;t=80691&amp;r=4968&amp;n=0&amp;att=1"><img src="templates/TM_Blue/images/lang_english/icon_yes.gif" alt="I ACCEPT the 'General Evaluator' role" title="I ACCEPT the 'General Evaluator' role" border="0"></a>
 
-<a href="kb.php?mode=article&amp;k=186" class="genmed">Ah Counter</a> */
+cron.schedule('*/5 * * * * *', () => {
+    console.log('running a task every 5 seconds');
+});
